b5377dc223c5dd949ebbdcbde93546d73ab5d616
StyearX <nazzz9442@gmail.com>
2026-06-22 00:31:33 +0800

    Add files via upload

--- Initiate.lua
+++ Initiate.lua
@@ -1,37 +1,40 @@
-
-local hidegui = getgenv().hideui or false
-local cloneref: () -> () = cloneref or function(...): (...any) -> (...any) return (...) end
-local httpservice = cloneref(game:GetService('HttpService'))
-local getasync: () -> () = function(string: string): (string) -> (string)
-    return game:HttpGet(string, true)
-end
-makefolder('Bloxstrap');
-makefolder('Bloxstrap/Main');
-    makefolder('Bloxstrap/Main/Functions');
-    makefolder('Bloxstrap/Main/Configs');
-    makefolder('Bloxstrap/Main/Fonts')
-    makefolder('Bloxstrap/Images')
-local install: () -> () = function(config: {path: string, setup: boolean}): (table) -> ()
-    config = config or {}
-    
-    for i: number, v: table in httpservice:JSONDecode(getasync('https://api.github.com/repos/qwertyui-is-back/Bloxstrap/contents/')) do
-        if v.name:find('.lua') then
-            writefile(`Bloxstrap/{v.name}`, `return loadstring(game:HttpGet('https://raw.githubusercontent.com/qwertyui-is-back/Bloxstrap/refs/heads/main/{v.name}', true))()`);
-        elseif v.name:find('.mp3') or v.name:find('.png') then
-            writefile(`Bloxstrap/{v.name}`, game:HttpGet(`https://raw.githubusercontent.com/qwertyui-is-back/Bloxstrap/refs/heads/main/{v.name}`));
-        end;
-    end;
-    writefile(`Bloxstrap/Main/Bloxstrap.lua`, `return loadstring(game:HttpGet('https://raw.githubusercontent.com/qwertyui-is-back/Bloxstrap/refs/heads/main/Main/Bloxstrap.lua', true))()`);
-    for i: number, v: table in httpservice:JSONDecode(getasync('https://api.github.com/repos/qwertyui-is-back/Bloxstrap/contents/Main/Functions')) do
-        writefile(`Bloxstrap/Main/Functions/{v.name}`, `return loadstring(game:HttpGet('https://raw.githubusercontent.com/qwertyui-is-back/Bloxstrap/refs/heads/main/Main/Functions/{v.name}', true))()`);
-    end;
-    writefile("Bloxstrap/Main/Configs/Default.json", "{}")
-end;
-
-if (not isfolder('Bloxstrap') or #listfiles('Bloxstrap') <= 6) then
-    install({})
+local hideui = getgenv().hideui or false
+local cloneref = cloneref or function(...) return ... end
+local httpService = cloneref(game:GetService("HttpService"))
+local getAsync = function(url)
+	return game:HttpGet(url, true)
 end
 
-local Bloxstrap: table = loadfile('Bloxstrap/Main/Bloxstrap.lua')()
-Bloxstrap.start() 
-Bloxstrap.Visible(not hidegui)
+makefolder("Cleostrap")
+makefolder("Cleostrap/Main")
+makefolder("Cleostrap/Main/Functions")
+makefolder("Cleostrap/Main/Configs")
+makefolder("Cleostrap/Main/Fonts")
+makefolder("Cleostrap/Images")
+makefolder("Cleostrap/Logs")
+
+local install = function()
+	for _, file in httpService:JSONDecode(getAsync("https://api.github.com/repos/StyearX/Cleostrap/contents/")) do
+		if file.name:find(".lua") then
+			writefile(`Cleostrap/{file.name}`, `return loadstring(game:HttpGet('https://raw.githubusercontent.com/StyearX/Cleostrap/refs/heads/main/{file.name}', true))()`)
+		elseif file.name:find(".mp3") or file.name:find(".png") then
+			writefile(`Cleostrap/{file.name}`, game:HttpGet(`https://raw.githubusercontent.com/StyearX/Cleostrap/refs/heads/main/{file.name}`))
+		end
+	end
+
+	writefile("Cleostrap/Main/Cleostrap.lua", `return loadstring(game:HttpGet('https://raw.githubusercontent.com/StyearX/Cleostrap/refs/heads/main/Main/Cleostrap.lua', true))()`)
+
+	for _, file in httpService:JSONDecode(getAsync("https://api.github.com/repos/StyearX/Cleostrap/contents/Main/Functions")) do
+		writefile(`Cleostrap/Main/Functions/{file.name}`, `return loadstring(game:HttpGet('https://raw.githubusercontent.com/StyearX/Cleostrap/refs/heads/main/Main/Functions/{file.name}', true))()`)
+	end
+
+	writefile("Cleostrap/Main/Configs/Default.json", "{}")
+end
+
+if not isfile("Cleostrap/Main/Cleostrap.lua") then
+	install()
+end
+
+local Cleostrap = loadfile("Cleostrap/Main/Cleostrap.lua")()
+Cleostrap.start()
+Cleostrap.Visible(not hideui)
--- README.md
+++ README.md
@@ -1,11 +1,7 @@
-# Bloxstrap
-A script that attempts to recreate Bloxstrap, so you can use FFlags without installing a program.
+# Cleostrap
+A script that lets you use FFlags without installing a program.
 
 ## Installation
 ```lua
-getgenv().autosetup = {
-    path = 'Bloxstrap', --> doesnt work rn
-    setup = true --> init after installaiton
-}
-loadstring(game.HttpGet(game, 'https://raw.githubusercontent.com/qwertyui-is-back/Bloxstrap/refs/heads/main/installer.lua', true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/StyearX/Cleostrap/refs/heads/main/Initiate.lua", true))()
 ```
