c3a962751cd3291190802e175a0b47f097f84234
StyearX <nazzz9442@gmail.com>
2026-06-28 17:26:27 +0800

    Update GuiLibrary.lua

--- Main/Functions/GuiLibrary.lua
+++ Main/Functions/GuiLibrary.lua
@@ -700,7 +700,7 @@ function cleolib:MakeWindow(Configs)
 	local MainFrame = InsertTheme(Create("ImageButton", ScreenGui, {
 		Size = UDim2.fromOffset(UISizeX, UISizeY),
 		Position = UDim2.new(0.5, -UISizeX/2, 0.5, -UISizeY/2),
-		BackgroundTransparency = 0.8,
+		BackgroundTransparency = 0.1,
 		Image = "rbxassetid://125683901243942",
 		ImageTransparency = 0,
 		ScaleType = Enum.ScaleType.Crop,
